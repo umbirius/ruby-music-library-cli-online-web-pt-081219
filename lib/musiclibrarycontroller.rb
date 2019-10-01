5420cbfa2645291034c5e2c3b1ae9c89f18f4418
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-01 14:15:31 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -14,14 +14,14 @@ class MusicLibraryController
     puts("To list all of the artists in your library, enter 'list artists'.")
     puts("To list all of the genres in your library, enter 'list genres'.")
     puts("To list all of the songs by a particular artist, enter 'list artist'.")
-    puts("To" list all of the songs of a particular genre, enter 'list genre'.")
+    puts("To list all of the songs of a particular genre, enter 'list genre'.")
     puts("To play a song, enter 'play song'.")
     puts("To quit, type 'exit'.")
     puts("What would you like to do?")
-    input = gets.strip"
+    input = ""
     while input != 'exit'
       input = gets.strip
-      if input == "'list songs'"
+      if input == 'list songs'
         list_songs
       elsif input == 'list artists'
         list_artists 
